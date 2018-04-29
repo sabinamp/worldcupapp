f1e4a2c65ed919e3ab9038da935ffbade4852b8e
sallaji <santiagollano4@gmail.com>
2018-04-29 08:36:40 +0200

    adding firebase credentials

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD8XEgUDp17a-anWF-OefaOwD_yF12mzH4",
+    authDomain: "ws6c-87e23.firebaseapp.com",
+    databaseURL: "https://ws6c-87e23.firebaseio.com",
+    projectId: "ws6c-87e23",
+    storageBucket: "ws6c-87e23.appspot.com",
+    messagingSenderId: "422724075244"
+};
